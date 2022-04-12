7f270838dbbfd3e5731b5796f60b688cea5334e4
alexander-angelov-tnei <alexander.angelov@tneigroup.com>
2022-04-12 12:07:29 +0100

    Creating IscInterface.

---
+++ IscInterface.py
@@ -0,0 +1,17 @@
+"""
+Lumache - Python library for cooks and food lovers.
+"""
+
+
+class IscInterface:
+    """
+    The IscInterface class is the main interface class used to access all other Ipsa objects and functions.
+    It must be created before any other references to Ipsa objects.
+    """
+    def ReadFile(strName: str):
+        """
+        Opens an Ipsa i2f file strName and returns an IscNetwork instance for that file.
+
+        :param strName (str): The Ipsa i2f file that is going to be opened.
+        :return (IscNetwork): The IscNetwork instance for the strName file
+        """
